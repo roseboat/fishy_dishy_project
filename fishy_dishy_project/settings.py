8082add09c0c2ac0fc44ba563c7303109e73fda7
Calvin Hew <2078855h@student.gla.ac.uk>
2018-03-17 17:46:45 +0000

    fixed everything.. added websites back again

--- fishy_dishy_project/settings.py
+++ fishy_dishy_project/settings.py
@@ -45,8 +45,8 @@ INSTALLED_APPS = [
     
 ]
 
-
-MIDDLEWARE = [
+#for django 1.9
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
