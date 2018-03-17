71d00b75acf9c2c1fef1528ffbd7655893f6904d
Rosa Downing <0908715d@student.gla.ac.uk>
2018-03-17 12:25:45 +0000

    meh

--- fishydishy/views.py
+++ fishydishy/views.py
@@ -286,16 +286,16 @@ def user_login(request):
 
 @login_required
 def user_profile(request):
-    u = User.objects.get(id=1)
-    u_p  = u.user_profile
+    profile = UserProfile.object.get(id=1)
+
     context_dict = {}
     recipe_list = Recipe.objects.filter(user=request.user.username)
 
     # userStuff = UserProfile.objects.get(user=request.user)
     # context_dict['userStuff'] = userStuff
 
-    userStuff = u_p
-    context_dict['userStuff'] = userStuff
+    # userStuff = u_p
+    context_dict['userStuff'] = profile
     context_dict['recipes'] = recipe_list
 
     return render(request, 'fishydishy/user_profile.html', context_dict)
